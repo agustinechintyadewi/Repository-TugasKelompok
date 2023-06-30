8beea3151d36ade3ce0d7b1076a91a5ca0a00d45
Komang Agustine Chintya Dewi <134162796+agustinechintyadewi@users.noreply.github.com>
2023-07-01 03:01:17 +0800

    Add files via upload

---
+++ database/migrations/2023_06_27_134231_create_hewans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hewans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_hewan');
+            $table->string('kode_hewan');
+            $table->string('jenis_hewan');
+            $table->string('umur_hewan');
+            $table->text('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hewans');
+    }
+};
